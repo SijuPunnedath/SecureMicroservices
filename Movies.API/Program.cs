d08d566c3a2da1572a1079de4be54eb76d731098
SijuPunnedath <sijukodanad@gmail.com>
2023-07-09 07:27:11 +0530

    Movie API Completed

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -10,8 +10,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
 var app = builder.Build();
+
+//static void SeedDatabae(IHost host)
+//{
+    using(var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var moviecontext = services.GetRequiredService<MoviesAPIContext>();
+        MoviesContextSeed.SeedAsync(moviecontext);  
+
+    }
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
